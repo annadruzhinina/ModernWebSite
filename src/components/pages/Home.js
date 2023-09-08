b2be069a8889708abcaefea490503c36eab09149
anna <anna@rcbd.org>
2023-09-08 15:23:25 -0400

    First commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection.js";
+
+function Home() {
+  console.log("Home component is rendering");
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
